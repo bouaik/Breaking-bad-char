ac2047abb60ca406b4d2ece7a51a71b21d6f6316
Lhoussaine <bouaik.lhou@gmail.com>
2020-10-22 21:32:21 +0100

    Create filter reducer

--- src/redux/character/reducers/index.js
+++ src/redux/character/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import characterReducer from "./characterReducer";
+import filterReducer from "./filterReducer";
 
 const rootReducer = combineReducers({
   characters: characterReducer,
+  filter: filterReducer,
 });
 
 export default rootReducer;
