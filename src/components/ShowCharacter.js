90371ec6d5f1b599f97e844d74d1f3444f80a54a
Lhoussaine <bouaik.lhou@gmail.com>
2020-10-23 00:47:48 +0100

    Style show character

--- src/components/ShowCharacter.js
+++ src/components/ShowCharacter.js
@@ -8,10 +8,10 @@ const ShowCharacter = ({ character }) => {
       {character.map((character) => (
         <div key={character.char_id} className="character-wrapper">
           <div className="img-and-name">
-            <h2>{character.name}</h2>
             <img src={character.img} alt="char" width="400px" />
           </div>
           <div className="char-details">
+            <h2>Name : {character.name}</h2>
             <h3>Birthday : {character.birthday}</h3>
             <h3>Nickname : {character.nickname}</h3>
             <h3>portrayed : {character.portrayed}</h3>
--- src/components/ShowCharacter.styled.js
+++ src/components/ShowCharacter.styled.js
@@ -3,8 +3,19 @@ import styled from "styled-components";
 export const ShowCharacterr = styled.div`
   margin-top: 50px;
   height: 97vh;
+  h2 {
+    text-align: center;
+  }
 
   .character-wrapper {
     display: flex;
+    justify-content: space-evenly;
+  }
+
+  .char-details {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
   }
 `;
